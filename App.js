8215256b5c3b8847a178e4a0ce97c3262e576b82
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2024-03-12 08:30:36 +0530

    added customtheme

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
-import Registration from './src/Registration'
+import { StyleSheet, View } from 'react-native';
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
+import Registration from './src/Registration';
+
+// Define custom theme
+const customTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#7f0404', // Corrected color value
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <PaperProvider theme={customTheme}>
+      <View style={styles.container}>
 
-       <Registration/>  
-    </View>
+        <Registration />
+      </View>
+    </PaperProvider>
   );
 }
 
