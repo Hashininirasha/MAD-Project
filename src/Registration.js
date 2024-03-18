59d622ae97b529d716a390eeed085c8427ba9e07
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2024-03-18 12:27:52 +0530

    added header

--- src/Registration.js
+++ src/Registration.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
-
+import Header from "./Components/Header";
 
 export default function Registration() {
   return (
+    <View>
+      <Header/>
     <View style={styles.container}>
     <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
     Press me
@@ -13,16 +15,16 @@ export default function Registration() {
   <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
     Press me
   </Button>
-  
+  </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    // flex: 1,
     justifyContent: 'center',
+    marginTop: 20,
+    padding: 30,
   },
 });
