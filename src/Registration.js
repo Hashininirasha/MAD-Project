bee05fd4e378178b48ad27b3b00f3222ff9456e9
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2024-03-26 09:35:05 +0530

    added styles in bottom in inputs

--- src/Registration.js
+++ src/Registration.js
@@ -9,6 +9,8 @@ export default function Registration() {
   const [firstname, setFirstname] = React.useState("");
   const [lastname, setLastname] = React.useState("");
   const [email, setEmail] = React.useState("");
+  const [password, setpassword] = React.useState("");
+  const [conpassword, setConpassword] = React.useState("");
   return (
 <View>
   <Header/>
@@ -18,19 +20,43 @@ export default function Registration() {
       label="FirstName"
       value={firstname}
       onChangeText={firstname => setFirstname(firstname)}
+      style={styles.input}
     />
 
 <TextInput
       label="FirstName"
       value={lastname}
       onChangeText={lastname => setLastname(lastname)}
+      style={styles.input}
     />
 
 <TextInput
       label="Email"
       value={email}
       onChangeText={email => setEmail(email)}
+      style={styles.input}
     />
+
+<TextInput
+      label="Password"
+      value={password}
+      onChangeText={password => setpassword(password)}
+      secureTextEntry
+      style={styles.input}
+    />
+
+<TextInput
+      label="Confirm Password"
+      value={conpassword}
+      onChangeText={conpassword => setConpassword(conpassword)}
+      secureTextEntry
+      style={styles.input}
+    />
+
+<Button mode="contained" style={styles.reg}>
+        Register
+    
+      </Button>
   </View>
   </View>
   );
@@ -43,4 +69,9 @@ const styles = StyleSheet.create({
     marginTop: 20,
     padding: 30,
   },
+  input: {
+    marginBottom: 30,
+  },reg:{
+    marginBottom: 100,
+  }
 });
