7c9d0e18b65c1c2a2a0f805b00cb3d71a8362639
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2024-03-26 09:00:35 +0530

    added inputs

--- src/Registration.js
+++ src/Registration.js
@@ -1,22 +1,38 @@
+import * as React from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
 import Header from "./Components/Header";
-
+import { TextInput } from 'react-native-paper'
 export default function Registration() {
+  const [firstname, setFirstname] = React.useState("");
+  const [lastname, setLastname] = React.useState("");
+  const [email, setEmail] = React.useState("");
   return (
-    <View>
-      <Header/>
-    <View style={styles.container}>
-    <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-    Press me
-  </Button>
+<View>
+  <Header/>
 
-  <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-    Press me
-  </Button>
+    <View style={styles.container}>
+    <TextInput
+      label="FirstName"
+      value={firstname}
+      onChangeText={firstname => setFirstname(firstname)}
+    />
+
+<TextInput
+      label="FirstName"
+      value={lastname}
+      onChangeText={lastname => setLastname(lastname)}
+    />
+
+<TextInput
+      label="Email"
+      value={email}
+      onChangeText={email => setEmail(email)}
+    />
   </View>
-    </View>
+  </View>
   );
 }
 
